97e833687d65db37da3e9827c761fb4ff14721e0
testcase-108 <krishnaforwork18@gmail.com>
2025-06-30 10:23:02 +0530

    also pushed model

---
+++ model/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication7.model
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public required string Title { get; set; }
+        public ICollection<StudentCourse>? StudentCourses { get; set; } = new List<StudentCourse>();
+    }
+}
---
+++ model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+namespace WebApplication7.model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public required string FirstName { get; set; }
+        public  string? LastName { get; set; }
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+    }
+}
